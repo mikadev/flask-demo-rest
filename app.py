38c4f60a45fbf559b62ab1c85568983b37d173b8
Elbaz Michael <elbazmichael92@gmail.com>
2023-06-14 15:09:58 +0200

    Group by users

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import json
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -24,17 +22,48 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL()
 mysql.init_app(app)
 
-
 # Required
+
+"""
+[
+    {
+        "email": "test@mail.fr",
+        "id": 1,
+        [ 
+            {
+                "tel": "1010101",
+                "ud.id": 1,
+                "user_id": 1
+            }, 
+             {
+                "tel": "00220020",
+                "ud.id": 1,
+                "user_id": 1
+            }, 
+        ]
+    }
+]
+"""
 
 
 @app.route('/')
 @app.route('/users')
-def users():
+def all_users():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT* FROM users''')
-    rv = cur.fetchall()
-    return json.dumps(rv)
+    cur.execute('''SELECT * FROM users u JOIN user_data ud ON u.id = ud.user_id''')
+    return jsonify(list(users(cur.fetchall()).values()))
+
+
+def users(users: dict) -> dict:
+    res = {}
+    for idx, user in enumerate(users):
+        email = user['email']
+        tel = user['tel']
+        if email in res:
+            res[email]['user_data'].append(tel)
+        else:
+            res[email] = {'email': email, 'user_data': [tel]}
+    return res
 
 
 @app.route('/data')
